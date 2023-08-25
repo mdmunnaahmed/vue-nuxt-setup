128033a73357fb577052ff682dc8ec41f628d8ff
mdmunnaahmed <msdmunna77@gmail.com>
2023-08-25 21:37:28 +0600

    nuxt install

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["~/assets/main.scss"],
+  postcss: {
+    postcssOptions: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+});
